d810aca2ee71f037c4c439a7bff3408e94d85b24
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-17 19:48:42 +0700

    Create monitor_service_restart.sh

---
+++ monitor_service_restart.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+while true
+do
+
+# Set up Telegram bot API and chat ID
+BOT_API_KEY="6360723418:AAF1aya50fEi6sVuOwhXp53IV19siP3gqLc"
+CHAT_ID="489842337"
+
+# Check if MySQL service is running  
+if ! systemctl is-active --quiet mysql.service; then
+  # If not running, restart and send message to Telegram
+    sudo service mysql restart
+    MESSAGE="MySQL Service was down. Restarting now"
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
+fi
+
+# Check if Nginx web server is running  (Thay bằng dịch vụ webserver trên VPS của bạn)
+if ! systemctl is-active --quiet nginx.service; then
+  # If not running, restart and send message to Telegram
+    sudo service mysql restart
+    MESSAGE="Nginx Service was down. Restarting now"
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
+fi
+
+# Check if PHP service is running (Thay bằng dịch vụ PHP trên VPS của bạn)
+if ! systemctl is-active --quiet php7.4-fpm.service; then
+  # If not running, restart and send message to Telegram
+    sudo service php7.4-fpm restart
+    MESSAGE="PHP Service was down. Restarting now"
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
+fi
+
+sleep 1
+done
